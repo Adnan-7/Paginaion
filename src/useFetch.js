b297c3cd7e32191795c6e23231761a83f72357e5
Adnan Khan <adnan7.zain@gmail.com>
2021-12-16 22:51:03 +0500

    pagination setup

---
+++ src/useFetch.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+const url = 'https://api.github.com/users/john-smilga/followers?per_page=100';
+
+export const useFetch = () => {
+  const getProducts = async () => {
+    const { data } = await axios.get(url);
+    return data;
+  };
+
+  const { data, isLoading } = useQuery('products', getProducts);
+
+  return { data, isLoading };
+};
